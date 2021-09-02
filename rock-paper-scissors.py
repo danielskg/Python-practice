acb331a0349ab13c2a9f4a43a7f4a8a65f316eca
danielskg <56683220+danielskg@users.noreply.github.com>
2021-09-02 18:21:22 +0300

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,11 +1,11 @@
-import random
-gamenum = 0
+ import random
+gamenum = player = pc = 0
 options = ['πέτρα', 'χαρτί', 'ψαλίδι']
 while True:
 
-    global options
-    global games
     gamenum += 1
+    points = f'Εσύ: {player} πόντους - Computer: {pc} πόντους'
+    print(points)
     pcchoice = options[random.randrange(0, 3)]
     command = 'π[έτρα], ψ[αλίδι], χ[ααααααααααρτί]! - [\q για να βγεις]'
     #print(pcchoice)
@@ -14,16 +14,18 @@ while True:
     userchoice = input(command)
 
     if (userchoice == '\q' or userchoice == '\;'):
-        print('\nΓεια σου ρε φίλε!')
+        print('\nΓειααααα σου ρε φίλε!')
         break
     
     elif userchoice in ['p', 'petra', 'rock', 'π', 'πέτρα', 'πετρα']:
         if pcchoice == options[0]:
-            print(f'Διάλεξες πέτρα - Διάλεξα {pcchoice}. Ισοπαλία.')
+            print(f'Διάλεξες πέτρα - Διάλεξα {pcchoice}. Ισοπαλία.' + points)
         elif pcchoice == options[1]:
             print(f'Διάλεξες πέτρα - Διάλεξα {pcchoice}. Σε κέρδισα.')
+            pc += 1
         else:
             print(f'Διάλεξες πέτρα - Διάλεξα {pcchoice}.  Έχασα.')
+            player += 1
         print('\n')
         
     elif userchoice in ['ps', 'psalidi', 'scissors', 'ψ', 'ψαλίδι', 'ψαλιδι', 'c']:
@@ -31,8 +33,10 @@ while True:
             print(f'Διάλεξες ψαλίδι - Διάλεξα {pcchoice}. Ισοπαλία.')
         elif pcchoice == options[0]:
             print(f'Διάλεξες ψαλίδι - Διάλεξα {pcchoice}. Σε κέρδισα.')
+            pc += 1
         else:
             print(f'Διάλεξες ψαλίδι - Διάλεξα {pcchoice}. Έχασα.')
+            player += 1
         print('\n')
         
     elif userchoice in ['x', 'xarti', 'paper', 'χ', 'χαρτί', 'χαρτι']:
@@ -40,8 +44,10 @@ while True:
             print(f'Διάλεξες χαρτί - Διάλεξα {pcchoice}. Ισοπαλία.')
         elif pcchoice == options[0]:
             print(f'Διάλεξες χαρτί - Διάλεξα {pcchoice}. Έχασα.')
+            player += 1
         else:
             print(f'Διάλεξες χαρτί - Διάλεξα {pcchoice}.  Σε κέρδισα.')
+            pc += 1
         print('\n')
         
     else:
